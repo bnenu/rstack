198bab0b72b67e474baf44710a5183a88a5301e3
bnenu <bogdan.nenu@gmail.com>
2016-05-06 00:10:25 +0300

    modified gitignore

--- src/app/components/home/home.js
+++ src/app/components/home/home.js
@@ -6,7 +6,13 @@ const Home = () => <div>
     New ...Home page is awesome
   </h1>
   <h1> And default style</h1>
-
+  <section>
+    <div className="row">
+      <div className="col-xs-6">
+        a better future
+      </div>
+    </div>
+  </section>
 </div>
 
 export default Home
